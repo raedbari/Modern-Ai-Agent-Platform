ded10dba71a295a25dcf9b7c8f975731eefd06c7
raedbari <raedbari203@gmail.com>
2026-08-04 07:51:36 +0300

    chore(frontend): disable webpack cache in development

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { dev }) {
+    if (dev) {
+      config.cache = false;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
